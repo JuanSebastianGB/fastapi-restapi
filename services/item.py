8b8d0c46965e10b79b18817b794470ad5996027b
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-05-01 07:27:44 -0500

    Adding docstring to services fucntions

--- services/item.py
+++ services/item.py
@@ -4,10 +4,32 @@ from models.schemas import ItemCreate
 
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
+    """
+    Retrieve items from the database.
+
+    Parameters:
+        - db (Session): The database session.
+        - skip (int): The number of items to skip in the query (default 0).
+        - limit (int): The maximum number of items to retrieve (default 100).
+
+    Returns:
+        - List[ItemModel]: A list of ItemModel objects.
+    """
     return db.query(ItemModel).offset(skip).limit(limit).all()
 
 
 def create_user_item(db: Session, item: ItemCreate, user_id: int):
+    """
+    Create a new item owned by a user in the database.
+
+    Parameters:
+        - db (Session): The database session.
+        - item (ItemCreate): The data for the new item.
+        - user_id (int): The ID of the user who will own the new item.
+
+    Returns:
+        - ItemModel: The newly created item object.
+    """
     db_item = ItemModel(**item.dict(), owner_id=user_id)
     db.add(db_item)
     db.commit()
--- services/user.py
+++ services/user.py
@@ -4,18 +4,59 @@ from models.schemas import UserCreate
 
 
 def get_user(db: Session, user_id: int):
+    """
+    Retrieve a user from the database by their ID.
+
+    Parameters:
+        - db (Session): The database session.
+        - user_id (int): The ID of the user to retrieve.
+
+    Returns:
+        - UserModel: The user object, if it exists in the database.
+    """
     return db.query(UserModel).filter(UserModel.id == user_id).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
+    """
+    Retrieve a list of users from the database.
+
+    Parameters:
+        - db (Session): The database session.
+        - skip (int): The number of users to skip.
+        - limit (int): The maximum number of users to retrieve.
+
+    Returns:
+        - List[UserModel]: A list of user objects.
+    """
     return db.query(UserModel).offset(skip).limit(limit).all()
 
 
 def get_user_by_email(db: Session, email: str):
+    """
+    Retrieve a user from the database by their email address.
+
+    Parameters:
+        - db (Session): The database session.
+        - email (str): The email address of the user to retrieve.
+
+    Returns:
+        - UserModel: The user object, if it exists in the database.
+    """
     return db.query(UserModel).filter(UserModel.email == email).first()
 
 
 def create_user(db: Session, user: UserCreate):
+    """
+    Create a new user in the database.
+
+    Parameters:
+        - db (Session): The database session.
+        - user (UserCreate): The user information to create.
+
+    Returns:
+        - None
+    """
     fake_password = user.password+"temporalpwd"
     db_user = UserModel(email=user.email, hashed_password=fake_password)
     db.add(db_user)
@@ -24,4 +65,14 @@ def create_user(db: Session, user: UserCreate):
 
 
 def get_user_items(db: Session, user_id: int):
+    """
+    Retrieve a user's items from the database.
+
+    Parameters:
+        - db (Session): The database session.
+        - user_id (int): The ID of the user whose items to retrieve.
+
+    Returns:
+        - List[ItemModel]: A list of item objects belonging to the user.
+    """
     return get_user(db, user_id).items()
